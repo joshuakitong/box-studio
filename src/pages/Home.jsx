11f5f6aad84bb27eeb1622ce8fd864468408276b
joshuakitong <joshuakitong@gmail.com>
2025-08-08 07:39:12 +0800

    feat: add page placeholders

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen max-w-4xl mx-auto">
+      <img className="w-64" src={`${import.meta.env.BASE_URL}box84studio.png`} alt="BOX84 Studio" />
+    </div>
+  );
+}

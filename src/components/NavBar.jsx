4c92babd1dccdb5728ced7a9bda1ce74f2067b48
joshuakitong <joshuakitong@gmail.com>
2025-08-08 08:35:35 +0800

    refactor: change navbar colors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,15 +14,15 @@ export default function NavBar() {
     <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
       <ul className="flex items-center space-x-4 md:space-x-16 sm:space-x-8 px-2 md:px-8 sm:px-4 py-2 rounded-full 
                      bg-black/10 backdrop-blur-md border-2 border-white 
-                     text-sm md:text-lg sm:text-md">
+                     text-sm md:text-lg sm:text-md font-semibold">
         {links.map((link) => (
           <li>
             <Link
               to={link.path}
-              className={`whitespace-nowrap transition-colors ${
+              className={`whitespace-nowrap transition-colors hover:text-white ${
                 location.pathname === link.path
-                  ? "text-blue-500 font-semibold"
-                  : "text-white hover:text-blue-400"
+                  ? "text-white"
+                  : "text-gray-400"
               }`}
             >
               {link.label}

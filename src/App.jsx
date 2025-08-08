070bbbb2f91538d08f94b64f7198e702242146dc
joshuakitong <joshuakitong@gmail.com>
2025-08-08 08:35:53 +0800

    feat: add animation when changing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+  const containerRef = useRef();
+
+  useLayoutEffect(() => {
+    gsap.fromTo(
+      containerRef.current,
+      { opacity: 0, scale: 0.95, filter: "blur(8px)" },
+      { opacity: 1, scale: 1, filter: "blur(0px)", duration: 0.7, ease: "power2.out" }
+    );
+  }, [location]);
+
   return (
-    <Router>
-      <div className="bg-black text-white">
-      <NavBar />
-      <Routes>
+    <div ref={containerRef}>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-black text-white">
+        <NavBar />
+        <AnimatedRoutes />
       </div>
     </Router>
   );

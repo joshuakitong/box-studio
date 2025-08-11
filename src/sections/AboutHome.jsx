233ed4b0786ebafaa85f40a21a62f6a61e459309
joshuakitong <joshuakitong@gmail.com>
2025-08-11 22:37:37 +0800

    refactor: remove comments, adjust padding, and add section title

--- src/sections/AboutHome.jsx
+++ src/sections/AboutHome.jsx
@@ -1,14 +1,13 @@
 export default function AboutHome() {
   return (
-    <div className="relative w-full py-16 px-6 sm:px-12 border-y border-white">
-      {/* Background image */}
+    <div className="relative w-full py-12 px-6 sm:px-12 border-y border-white">
+      <h2 className="relative z-10 mb-6 text-2xl sm:text-3xl font-bold text-center">About Us</h2>
       <img
         src={`${import.meta.env.BASE_URL}about/aboutbackground.jpg`}
         alt="Background"
         className="absolute inset-0 w-full h-full object-cover object-left sm:object-center"
       />
 
-      {/* Overlay for border, rounding, and text */}
       <div className="relative w-full max-w-xl mx-auto rounded-4xl overflow-hidden border border-white/30">
         <div className="bg-black/84 backdrop-blur-sm text-white px-4 py-6">
           <p className="text-sm sm:text-md md:text-lg lg:text-xl text-center">

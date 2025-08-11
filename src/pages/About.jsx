3f48804175544c6b227498c648720653ab125a0a
joshuakitong <joshuakitong@gmail.com>
2025-08-12 05:27:10 +0800

    refactor: adjust about page layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,9 @@ import ReleasesSection from "../sections/Releases";
 
 export default function About() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col w-full pt-12">
       <AboutSection />
       <ReleasesSection />
-    </>
+    </div>
   );
 }

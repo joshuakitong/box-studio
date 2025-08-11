55c1fde23d0030fad8b5f50ffe15e2a32d1f3d9d
joshuakitong <joshuakitong@gmail.com>
2025-08-12 06:16:18 +0800

    feat: add about page background

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,12 @@ import ReleasesSection from "../sections/Releases";
 
 export default function About() {
   return (
-    <div className="min-h-screen flex flex-col w-full pt-12">
+    <div
+      className="min-h-screen flex flex-col w-full pt-12 bg-fixed bg-center bg-cover"
+      style={{
+        backgroundImage: `url(${import.meta.env.BASE_URL}about/aboutpagebackground.jpg)`,
+      }}
+    >
       <AboutSection />
       <ReleasesSection />
     </div>
